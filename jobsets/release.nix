f69e91edbfd3ce6d1a5e759fbeff5f5f51ea0108
Drew Hess <dhess-src@quixoftic.com>
2018-02-01 22:56:12 -0800

    Add job aggregates to release.nix.

--- jobsets/release.nix
+++ jobsets/release.nix
@@ -21,11 +21,40 @@ with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
 
 let
 
+  ## Aggregates are handy for defining jobs (especially for subsets of
+  ## platforms), but they don't provide very useful information in
+  ## Hydra, especially when they die. We use aggregates here to define
+  ## set of jobs, and then splat them into the output attrset so that
+  ## they're more visible in Hydra.
+
+  enumerateConstituents = aggregate: lib.listToAttrs (
+    map (d:
+           let
+             name = (builtins.parseDrvName d.name).name;
+             system = d.system;
+           in
+             { name = name + "." + system;
+               value = d;
+             }
+         )
+        aggregate.constituents
+  );
+
   jobs = {
+
+    cleanSources = pkgs.releaseTools.aggregate {
+      name = "nixpkgs-lib-quixoftic-cleanSources";
+      meta.description = "nixpkgs-lib-quixoftic cleanSources tests";
+      constituents = with jobs; [
+        cleanSourceNix.x86_64-linux
+      ];
+    };
+
   }
   // (mapTestOn (packagePlatforms pkgs));
 
 in
 {
-  inherit (jobs) cleanSourceNix;
+  inherit (jobs) cleanSources;
 }
+// enumerateConstituents jobs.cleanSources
