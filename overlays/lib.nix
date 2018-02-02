a108d1e28c8351d77c0b6c56d9fd997c828366c3
Drew Hess <dhess-src@quixoftic.com>
2018-02-02 01:07:39 -0800

    Move comments in lib.nix next to the code, at the top of the file.

--- overlays/lib.nix
+++ overlays/lib.nix
@@ -2,6 +2,23 @@ self: super:
 
 let
 
+  ## Functions for cleaning local source directories. These are useful
+  ## for filtering out files in your local repo that should not
+  ## contribute to a Nix hash, so that you can just `src = ./.` in
+  ## your derivation, and then filter that attribute after the fact.
+  ##
+  ## Note that these functions are composable, e.g., cleanSourceNix
+  ## (cleanCabalStack ...) is a valid expression. They will also
+  ## compose with any `lib.cleanSourceWith` function (but *not* with
+  ## `builtins.filterSource`; see the `lib.cleanSourceWith`
+  ## documentation).
+    
+
+  # In most cases, I believe that filtering Nix files from the source
+  # hash is the right thing to do. They're obviously already evaluated
+  # when a nix-build command is executed, so if *what they evaluate*
+  # changes they'll cause a rebuild anyway, as they should; while
+  # cosmetic changes (comments, formatting, etc.) won't.
   filterNix = name: type: let baseName = baseNameOf (toString name); in ! (
     type != "directory" && (
       super.lib.hasSuffix ".nix" baseName
@@ -9,6 +26,8 @@ let
   );
   cleanSourceNix = src: super.lib.cleanSourceWith { filter = filterNix; inherit src; };
 
+
+  # Clean Haskell projects.
   filterHaskell = name: type: let baseName = baseNameOf (toString name); in ! (
     type == "directory" && (
       baseName == ".cabal-sandbox" ||
@@ -24,6 +43,8 @@ let
   );
   cleanSourceHaskell = src: super.lib.cleanSourceWith { filter = filterHaskell; inherit src; };
 
+
+  # Clean system cruft, e.g., .DS_Store files on macOS filesystems.    
   filterSystemCruft = name: type: let baseName = baseNameOf (toString name); in ! (
     type != "directory" && (
       baseName == ".DS_Store"
@@ -31,6 +52,8 @@ let
   );
   cleanSourceSystemCruft = src: super.lib.cleanSourceWith { filter = filterSystemCruft; inherit src; };
 
+
+  # Clean files related to editors and IDEs.
   filterEditors = name: type: let baseName = baseNameOf (toString name); in ! (
     type != "directory" && (
       baseName == ".dir-locals.el"                         ||
@@ -47,6 +70,8 @@ let
   );
   cleanSourceEditors = src: super.lib.cleanSourceWith { filter = filterEditors; inherit src; };
 
+
+  # Clean maintainer files that don't affect Nix builds.
   filterMaintainer = name: type: let baseName = baseNameOf (toString name); in ! (
     type != "directory" && (
       # Note: .git can be a file when it's in a submodule directory
@@ -60,6 +85,9 @@ let
   );
   cleanSourceMaintainer = src: super.lib.cleanSourceWith { filter = filterMaintainer; inherit src; };
 
+
+  # A cleaner that combines all of the cleaners defined here, plus
+  # `lib.cleanSource` from Nixpkgs.
   cleanSourceAllExtraneous = src:
     cleanSourceMaintainer
       (cleanSourceEditors
@@ -68,6 +96,14 @@ let
             (cleanSourceNix
               (super.lib.cleanSource src)))));
 
+
+  # Clean the `src` attribute of a package. This is convenient when
+  # you use tools like `cabal2nix` to generate Nix files for local
+  # source repos, as these tools generally lack the ability to
+  # apply the various `clean*Source` functions to the `src`
+  # attribute that they generate. Instead, you can apply this
+  # function, plus one or more source cleaners, to a package that
+  # is the result of a `callPackage` function application.
   cleanPackage = cleanSrc: pkg: (pkg.overrideAttrs (oldAttrs: {
     src = cleanSrc oldAttrs.src;
   }));
@@ -75,49 +111,13 @@ let
 in
 {
   lib = (super.lib or {}) // {
-
-    ## Functions for cleaning local source directories. These are
-    ## useful for filtering out files in your local repo that should
-    ## not contribute to a Nix hash, so that you can just `src = ./.`
-    ## in your derivation, and then filter that attribute after the
-    ## fact.
-    ##
-    ## Note that these functions are composable, e.g., cleanSourceNix
-    ## (cleanCabalStack ...) is a valid expression. They will also
-    ## compose with any `lib.cleanSourceWith` function (but *not* with
-    ## `builtins.filterSource`; see the `lib.cleanSourceWith`
-    ## documentation).
-    
-    # In most cases, I believe that filtering Nix files from the source
-    # hash is the right thing to do. They're obviously already evaluated
-    # when a nix-build command is executed, so if *what they evaluate*
-    # changes they'll cause a rebuild anyway, as they should; while
-    # cosmetic changes (comments, formatting, etc.) won't.
     inherit cleanSourceNix;
-
-    # Clean Haskell projects.
     inherit cleanSourceHaskell;
-
-    # Clean system cruft, e.g., .DS_Store files on macOS filesystems.    
     inherit cleanSourceSystemCruft;
-
-    # Clean files related to editors and IDEs.
     inherit cleanSourceEditors;
-
-    # Clean maintainer files that don't affect Nix builds.
     inherit cleanSourceMaintainer;
-
-    # A cleaner that combines all of the cleaners defined here, plus
-    # `lib.cleanSource` from Nixpkgs.
     inherit cleanSourceAllExtraneous;
 
-    # Clean the `src` attribute of a package. This is convenient when
-    # you use tools like `cabal2nix` to generate Nix files for local
-    # source repos, as these tools generally lack the ability to
-    # apply the various `clean*Source` functions to the `src`
-    # attribute that they generate. Instead, you can apply this
-    # function, plus one or more source cleaners, to a package that
-    # is the result of a `callPackage` function application.
     inherit cleanPackage;
   };
 }
