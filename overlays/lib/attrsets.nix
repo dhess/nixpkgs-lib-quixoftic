27c6cd9d1d3677e4663aaab0e87acd68a917e4ab
Drew Hess <dhess-src@quixoftic.com>
2018-07-19 17:24:08 -0700

    Add some examples for attrsets operations.

--- overlays/lib/attrsets.nix
+++ overlays/lib/attrsets.nix
@@ -10,23 +10,65 @@ let
 in rec
 {
 
-  ## Given an attrset, return true if `pred` is true for each value in
-  ## the set, else false. Here `pred` is a predicate function of one
-  ## argument (the value of each attribute in the set).
+  ## Tests on attrsets.
+
+  /* Given an attrset, return true if `pred` is true for each value in
+     the set, else false. Here `pred` is a predicate function of one
+     argument (the value of each attribute in the set).
+
+     Example:
+       s1 = { a = { valid = true; }; b = { valid = true; }; }
+       allAttrs (v: v.valid) s1
+       => true
+       s2 = { a = { valid = true; }; b = { valid = false; }; }
+       allAttrs (v: v.valid) s2
+       => false
+       z = {};
+       allAttrs (v: v.valid) z
+       => true
+  */
 
   allAttrs = pred: attrs:
     lib.all pred (lib.mapAttrsToList (_: value: value) attrs);
 
-  ## Given an attrset, return true if `pred` is true for any value in
-  ## the set, else false. Here `pred` is a predicate function of one
-  ## argument (the value of each attribute in the set).
+
+  /* Given an attrset, return true if `pred` is true for any value in
+     the set, else false. Here `pred` is a predicate function of one
+     argument (the value of each attribute in the set).
+
+     Example:
+       s1 = { a = { valid = true; }; b = { valid = false; }; }
+       anyAttrs (v: v.valid) s1
+       => true
+       s2 = { a = { valid = false; }; b = { valid = false; }; }
+       anyAttrs (v: v.valid) s2
+       => false
+       z = {};
+       anyAttrs (v: v.valid) z
+       => false
+  */
 
   anyAttrs = pred: attrs:
     lib.any pred (lib.mapAttrsToList (_: value: value) attrs);
 
-  ## Given an attrset, return true if `pred` is true for no value in
-  ## the set, else false. Here `pred` is a predicate function of one
-  ## argument (the value of each attribute in the set).
+
+  /* Given an attrset, return true if `pred` is true for no value in
+     the set, else false. Here `pred` is a predicate function of one
+     argument (the value of each attribute in the set).
+
+     Note: this is the same as !anyAttrs.
+
+     Example:
+       s1 = { a = { valid = true; }; b = { valid = false; }; }
+       noAttrs (v: v.valid) s1
+       => false
+       s2 = { a = { valid = false; }; b = { valid = false; }; }
+       noAttrs (v: v.valid) s2
+       => true
+       z = {};
+       noAttrs (v: v.valid) z
+       => true
+  */
 
   noAttrs = pred: attrs: !(anyAttrs pred attrs);
 
