db8a1473739c87e5256956c5c5f2adba38d53707
Drew Hess <src@drewhess.com>
2018-12-31 18:59:36 -1000

    Add a haskell.lib function to properly extend/override haskellPackages.
    An attempt to fix https://github.com/NixOS/nixpkgs/issues/26561.

--- overlays/haskell-lib.nix
+++ overlays/haskell-lib.nix
@@ -1,13 +1,29 @@
 self: super:
 
+let
+
+  # A proper super.haskellPackage.extend that fixes
+  # https://github.com/NixOS/nixpkgs/issues/26561.
+  #
+  # Note that f takes super: self: arguments, scoped within the
+  # Haskell package set hp.
+
+  properExtend = hp: f: hp.override (oldArgs: {
+    overrides =
+    super.lib.composeExtensions (oldArgs.overrides or (_: _: {}))
+      f;
+  });
+
+in
+
 {
   haskell = (super.haskell or {}) // {
-
     lib = (super.haskell.lib or {}) // {
+      inherit properExtend;
 
       ## Sometimes you don't want any haddocks to be generated for an
       ## entire package set, rather than just a package here or there.
-      noHaddocks = hp: (hp.extend (self: super: (
+      noHaddocks = hp: (properExtend hp (self: super: (
         {
           mkDerivation = args: super.mkDerivation (args // {
             doHaddock = false;
