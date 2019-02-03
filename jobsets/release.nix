18a538f54084ddc261b4d08c2ae4f70ec48eb344
Drew Hess <src@drewhess.com>
2019-02-03 19:36:47 +0000

    When you realize you're doing it wrong.
    Basically, I misunderstood the intended use of releaseTools.aggregate.
    Just enumerate all the jobs we want to run.

--- jobsets/release.nix
+++ jobsets/release.nix
@@ -19,105 +19,28 @@ with import (fixedNixPkgs + "/pkgs/top-level/release-lib.nix") {
   inherit supportedSystems scrubJobs nixpkgsArgs;
 };
 
-let
+(mapTestOn (rec {
+  nlqCleanSourceNix = all;
+  nlqCleanSourceHaskell = all;
+  nlqCleanSourceSystemCruft = all;
+  nlqCleanSourceEditors = all;
+  nlqCleanSourceMaintainer = all;
+  nlqCleanSourceAllExtraneous = all;
+  nlqCleanPackageNix = all;
+  nlqCleanPackageHaskell = all;
+  nlqCleanPackageSystemCruft = all;
+  nlqCleanPackageEditors = all;
+  nlqCleanPackageMaintainer = all;
+  nlqCleanPackageAllExtraneous = all;
+  nlqAttrSets = all;
+  nlqIPAddr = all;
+  nlqMisc = all;
+  nlqFfdhe = all;
+  nlqTypes = all;
+}))
+// {
 
-  all = pkg: map (system: pkg.${system}) supportedSystems;
-
-  jobs = {
-
-    cleanSources = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-cleanSources";
-      meta.description = "nixpkgs-lib-quixoftic cleanSource tests";
-      constituents = with jobs; [
-        nlqCleanSourceNix.x86_64-linux
-        (all nlqCleanSourceHaskell)
-        (all nlqCleanSourceSystemCruft)
-        (all nlqCleanSourceEditors)
-        (all nlqCleanSourceMaintainer)
-        (all nlqCleanSourceAllExtraneous)
-      ];
-    };
-
-
-    ## Note that these jobs should evaluate to the same values as their
-    ## namesakes in cleanSources.
-
-    cleanPackages = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-cleanPackages";
-      meta.description = "nixpkgs-lib-quixoftic cleanPackage tests";
-      constituents = with jobs; [
-        (all nlqCleanPackageNix)
-        (all nlqCleanPackageHaskell)
-        (all nlqCleanPackageSystemCruft)
-        (all nlqCleanPackageEditors)
-        (all nlqCleanPackageMaintainer)
-        (all nlqCleanPackageAllExtraneous)
-      ];
-    };
-
-    attrsets = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-attrsets";
-      meta.description = "nixpkgs-lib-quixoftic attrsets tests";
-      constituents = with jobs; [
-        (all nlqAttrSets)
-      ];
-    };
-
-    ipaddr = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-ipaddr";
-      meta.description = "nixpkgs-lib-quixoftic ipaddr tests";
-      constituents = with jobs; [
-        (all nlqIPAddr)
-      ];
-    };
-
-    misc = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-misc";
-      meta.description = "nixpkgs-lib-quixoftic miscellaneous tests";
-      constituents = with jobs; [
-        (all nlqMisc)
-      ];
-    };
-
-    security = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-security";
-      meta.description = "nixpkgs-lib-quixoftic security tests";
-      constituents = with jobs; [
-        (all nlqFfdhe)
-      ];
-    };
-
-    types = pkgs.releaseTools.aggregate {
-      name = "nixpkgs-lib-quixoftic-types";
-      meta.description = "nixpkgs-lib-quixoftic types tests";
-      constituents = with jobs; [
-        (all nlqTypes)
-      ];
-    };
-
-  }
-  // (mapTestOn ((packagePlatforms pkgs) // rec {
-
-    # This doesn't generate any actual jobs, we just want to make sure
-    # it evaluates properly.
-    haskell = packagePlatforms pkgs.haskell;
-  }));
-
-in
-{
-  inherit (jobs) cleanSources;
-  inherit (jobs) cleanPackages;
-  inherit (jobs) haskell;
-  inherit (jobs) attrsets;
-  inherit (jobs) ipaddr;
-  inherit (jobs) misc;
-  inherit (jobs) security;
-  inherit (jobs) types;
+  # This doesn't generate any actual jobs, we just want to make sure
+  # it evaluates properly.
+  haskell = packagePlatforms pkgs.haskell;
 }
-// pkgs.lib.testing.enumerateConstituents jobs.cleanSources
-// pkgs.lib.testing.enumerateConstituents jobs.cleanPackages
-// pkgs.lib.testing.enumerateConstituents jobs.attrsets
-// pkgs.lib.testing.enumerateConstituents jobs.ipaddr
-// pkgs.lib.testing.enumerateConstituents jobs.misc
-// pkgs.lib.testing.enumerateConstituents jobs.security
-// pkgs.lib.testing.enumerateConstituents jobs.types
--- overlays/lib/testing.nix
+++ overlays/lib/testing.nix
@@ -4,28 +4,11 @@ self: super:
 
 let
 
-  # Aggregates are handy for defining jobs (especially for subsets of
-  # platforms), but they don't provide very useful information in
-  # Hydra, especially when they die. We use aggregates here to define
-  # set of jobs, and then splat them into the output attrset so that
-  # they're more visible in Hydra.
-  #
-  # Note that each constituent may be either a list or an atom.
-  enumerateConstituents = aggregate: super.lib.listToAttrs (
-    map (value:
-           let
-             drvName = (builtins.parseDrvName value.name).name;
-             system = value.system;
-             name = drvName + "_" + system;
-           in { inherit name value; })
-        (super.lib.flatten aggregate.constituents)
-  );
-
 in
 {
   lib = (super.lib or {}) // {
     testing = (super.lib.testing or {}) // {
-      inherit enumerateConstituents;
+      enumerateSystems = pkg: systems: map (system: pkg.${system}) systems;
     };
   };
 }
