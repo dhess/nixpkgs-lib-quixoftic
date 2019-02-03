3eb43dd2626fd54109a8bb082c8266e6ea4b55cb
Drew Hess <src@drewhess.com>
2019-02-03 17:54:10 +0000

    Fix enumerateConstituents.

--- overlays/lib/testing.nix
+++ overlays/lib/testing.nix
@@ -3,21 +3,17 @@
 self: super:
 
 let
+
   # Aggregates are handy for defining jobs (especially for subsets of
   # platforms), but they don't provide very useful information in
   # Hydra, especially when they die. We use aggregates here to define
   # set of jobs, and then splat them into the output attrset so that
   # they're more visible in Hydra.
   enumerateConstituents = aggregate: super.lib.listToAttrs (
-    map (d:
+    map (value:
            let
-             name = (builtins.parseDrvName d.name).name;
-             system = d.system;
-           in
-             { name = name + "." + system;
-               value = d;
-             }
-         )
+             name = (builtins.parseDrvName value.name).name;
+           in { inherit name value; })
         aggregate.constituents
   );
 
