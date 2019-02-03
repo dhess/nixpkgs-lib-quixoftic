c4d1c0c1d1794b3c240fd4eaf9934555c5f5f8f2
Drew Hess <src@drewhess.com>
2019-02-03 18:56:37 +0000

    Fix enumerateConstituents so that it handles lists.

--- overlays/lib/testing.nix
+++ overlays/lib/testing.nix
@@ -9,10 +9,17 @@ let
   # Hydra, especially when they die. We use aggregates here to define
   # set of jobs, and then splat them into the output attrset so that
   # they're more visible in Hydra.
+  #
+  # Note that each constituent may be either a list or an atom.
   enumerateConstituents = aggregate: super.lib.listToAttrs (
-    map (value:
+    map (d:
            let
-             name = (builtins.parseDrvName value.name).name;
+             x = if builtins.isList d then builtins.head d else d;
+             name = (builtins.parseDrvName x.name).name;
+             value = super.releaseTools.aggregate {
+               inherit name;
+               constituents = super.lib.singleton d;
+             };
            in { inherit name value; })
         aggregate.constituents
   );
