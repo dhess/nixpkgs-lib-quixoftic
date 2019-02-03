83b060c8689373f8b7226dc8aa06ec653ca5560e
Drew Hess <src@drewhess.com>
2019-02-03 12:02:30 +0000

    Add NUR-style overlays to the package set.

--- package-set.nix
+++ package-set.nix
@@ -6,9 +6,18 @@ in
 { pkgs ? nixpkgs {} }:
 
 with pkgs.lib;
+
 let
+
   self = foldl'
     (prev: overlay: prev // (overlay (pkgs // self) (pkgs // prev)))
     {} (map import (import ./overlays.nix));
+
 in
-self
+self //
+{
+  overlays = {
+    lib = self.lib.importDirectory ./overlays/lib;
+    haskell = self.lib.importDirectory ./overlays/haskell;
+  };
+}
